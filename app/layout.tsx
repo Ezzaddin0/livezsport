c3baa624dd61147e62e3cc706f7ef94b83c1d604
Ezzaddin0 <ezoozm24@gmail.com>
2023-11-24 18:24:46 +0300

    add google adsanse

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {process.env.NODE_ENV === "production" && (
+
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9613838390292158"
+          crossOrigin="anonymous">
+          </script>
+        )}
+      </head>
       <body>
         <GoogleAnalytics/>
         <Provider>
